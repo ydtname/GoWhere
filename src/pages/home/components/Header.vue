2ed79c5d7c517bb42dfeb13f43410f8735928a51
ydt <468602953@qq.com>
2018-10-11 22:45:30 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <span class="iconfont icon-fanhui back-icon"></span>
+      </div>
+      <div class="header-input">
+        <span class="iconfont icon-sousuo"></span>
+        输入城市/景点/游玩主题
+      </div>
+      <div class="header-right">
+        城市
+        <span class="iconfont icon-sanjiaodown arrow-icon"></span>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeHeader"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varubkes.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    color: #fff
+    background-color: $bgColor
+    .header-left
+      width: .64rem
+      padding-left: .15rem
+      float: left
+      .back-icon
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      padding-left: .2rem
+      margin-top: .12rem
+      margin-left: .2rem
+      border-radius: .1rem
+      background-color: #fff
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .24rem
+
+</style>
