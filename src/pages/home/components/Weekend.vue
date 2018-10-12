8a27f3ef13a6400a828b324d6756888a2f38588f
ydt <468602953@qq.com>
2018-10-12 21:28:10 +0800

    add index-weekend && correct index-recommend picture path

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div class="title">热销推荐</div>
+    <div class="title">周末去哪儿</div>
     <ul>
       <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
-        <img :src="item.imgUrl" alt="" class="item-img">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" alt="" class="item-img">
+        </div>
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
-          <button class="item-button">查看详情</button>
         </div>
       </li>
     </ul>
@@ -21,17 +22,17 @@
       return {
         recommendList: [{
           id: '0001',
-          imgUrl: '/static/recommend.jpg',
+          imgUrl: '/static/weekend.jpg',
           title: '大连圣亚海洋世界',
           desc: '浪漫大连首站，浪漫的海洋主题乐园'
         }, {
           id: '0002',
-          imgUrl: '/static/recommend.jpg',
+          imgUrl: '/static/weekend.jpg',
           title: '大连圣亚海洋世界',
           desc: '浪漫大连首站，浪漫的海洋主题乐园'
         }, {
           id: '0003',
-          imgUrl: '/static/recommend.jpg',
+          imgUrl: '/static/weekend.jpg',
           title: '大连圣亚海洋世界',
           desc: '浪漫大连首站，浪漫的海洋主题乐园'
         }]
@@ -47,31 +48,20 @@
     line-height: .8rem
     background-color: #eee
     text-indent: .2rem
-  .item
+  .item-img-wrapper
     overflow: hidden
-    display: flex
-    height: 1.9rem
-    .item-img
-      width: 1.7rem
-      height: 1.7rem
-      padding: .1rem
-    .item-info
-      flex: 1
-      padding: .1rem
-      min-width: 0
-      .item-title
-        line-height: .54rem
-        font-size: .32rem
-        ellipsis()
-      .item-desc
-        line-height: .4rem
-        color: #ccc
-        ellipsis()
-      .item-button
-        line-height: .4rem
-        margin-top: .16rem
-        background-color: #ff9300
-        padding: 0 .2rem
-        border-radius .06rem
-        color: #fff
+    width: 100%
+    height: 33.9%
+  .item-img
+    width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
 </style>
