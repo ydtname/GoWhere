7734a6d66e3484b8e407933b809b888f14fa53a1
ydt <468602953@qq.com>
2018-10-12 11:32:19 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img"  :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data(){
+          return {
+            swiperOption: {
+              pagination: '.swiper-pagination',
+              loop: true
+            },
+            swiperList: [{
+              id: '0001',
+              imgUrl: '/static/swiper1.jpg'
+            },
+            {
+              id: '0002',
+              imgUrl: '/static/swiper2.jpg'
+            }]
+          }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff !important
+  .wrapper
+    width: 100%
+    height: 0
+    padding-bottom: 56%
+    overflow: hidden
+    background-color: #fff
+    .swiper-img
+      width: 100%
+      height: 3.6rem
+</style>
