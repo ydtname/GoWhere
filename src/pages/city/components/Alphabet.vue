c2de495c3fc917900de8abc6d7aed88999683ca8
ydt <468602953@qq.com>
2018-10-13 22:11:23 +0800

    performance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,14 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) in cities" :key="key">{{key}}</li>
+    <li
+      class="item"
+      v-for="item in letters"
+      :key="item"
+      :ref="item"
+      @touchmove="handleTouchMove"
+      @click="handleLetterClick">
+      {{item}}
+    </li>
   </ul>
 </template>
 
@@ -9,6 +17,43 @@
     name: "CityAlphabet",
     props: {
       cities: Object
+    },
+    computed: {
+      letters(){
+        const letters = []
+        for(let i in this.cities){
+          letters.push(i)
+        }
+        return letters
+      }
+    },
+    data(){
+      return {
+        // touchStatus: false
+        startY: 0,
+        timer: null
+      }
+    },
+    updated(){
+      //当ajax请求完成后并开始渲染，updatad执行将startY进行更新
+      this.startY = this.$refs['A'][0].offsetTop
+    },
+    methods: {
+      handleLetterClick(e){
+        this.$emit('change', e.target.innerText)
+      },
+      handleTouchMove(e){
+        if(this.timer) {
+          clearTimeout(this.timer) //节流，清除上一个异步执行函数
+        }
+        this.timer = setTimeout(()=>{
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 21)
+          if(index >= 0 && index < this.letters.length){
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      },
     }
   }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -19,7 +19,7 @@
         </div>
       </div>
 
-      <div class="area" v-for="(item, key) in cities">
+      <div class="area" v-for="(item, key) in cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom"
@@ -37,10 +37,19 @@
     name: "CityList",
     props: {
       cities: Object,
-      hot: Array
+      hot: Array,
+      letter: String
     },
     mounted(){
       this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+    watch: {
+      letter(){
+        if(this.letter){
+          const element = this.$refs[this.letter][0]
+          this.scroll.scrollToElement(element)
+        }
+      }
     }
   }
 </script>
